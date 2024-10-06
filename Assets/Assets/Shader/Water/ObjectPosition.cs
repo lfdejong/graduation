b84b207d3683b1f6ab81e452a5cef1e3d132f8ad
Levi <1006554@hr.nl>
2024-10-06 12:25:53 +0200

    Demo complete

---
+++ Assets/Assets/Shader/Water/ObjectPosition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_ObjectPosition", new Vector4(this.transform.position.x, this.transform.position.y, this.transform.position.z, this.transform.localScale.x));
+
+    }
+}
